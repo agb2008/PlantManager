0f33e0cc5a48ba652b519f5ba6d80702365b2ce1
levkotman <leoktmn@yandex.ru>
2021-05-14 19:11:53 +0200

    added vcalendar

---
+++ frontend/plantmanager/src/components/Calendar/Calendar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        this is calendar
+        <VCalendar />
+        <DatePicker />
+    </div>
+</template>
+
+<script>
+import VCalendar from 'v-calendar/lib/components/calendar.umd';
+import DatePicker from 'v-calendar/lib/components/date-picker.umd';
+
+    export default {
+        name: "Calendar",
+        components: {
+            VCalendar,
+            DatePicker
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
