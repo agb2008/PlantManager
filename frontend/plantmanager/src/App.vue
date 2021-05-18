625c97cb8d583cf5bddba0f82d310abf3f7fa129
archi <archiprigoda@gmail.com>
2021-05-18 17:47:43 +0300

    Revert "Revert "Inventory list outline create""
    This reverts commit 62b83a4a7064d9a3dfea670f806d20b11727ad7d.

--- frontend/plantmanager/src/App.vue
+++ frontend/plantmanager/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <InventoryList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import InventoryList from './components/InventoryList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    InventoryList,
   }
 }
 </script>
---
+++ frontend/plantmanager/src/components/InventoryList.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="inventory-list wrapper">
+		<h1>Инвентарий пользователя {{ user }} </h1>
+		<tabs>
+			<tab name="Seeds" :selected="true">
+				<h1>Семена</h1>
+				<p>Какой-то контент</p>
+			</tab>
+			<tab name="Seedling">
+				<h1>Рассады</h1>
+			</tab>
+			<tab name="Fertilizers">
+				<h1>Удобрения</h1>
+			</tab>
+		</tabs>
+	</div>
+</template>
+
+<script>
+
+import Tab from './Tab.vue'
+import Tabs from './Tabs.vue'
+
+export default {
+  components: {
+    Tab,
+    Tabs
+  },
+  props: {
+    msg: String
+  },
+
+  data() {
+        
+        return {
+            user: 'User1'
+        };
+        
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper {
+    width: 100%;
+    min-height: 100vh;
+    background-color: #f8f8f8;
+    margin: 0;
+    padding: 20px;
+  }
+</style>
