590f752ac4802d195e5ee2a0c35d4c51f8eb3cb3
agb2008 <64597413+agb2008@users.noreply.github.com>
2021-05-21 14:38:53 +0300

    Structure of backend project changed to SPA model
    Структура проекта была изменена на SPA

---
+++ backend/PlantManager/resources/js/components/Logout.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <div>
+        <h1 class="font-normal text-3xl leading-none">Выход из системы</h1>
+    </div>
+
+</template>
+
+<script>
+
+    export default {};
+
+</script>
---
+++ backend/PlantManager/resources/js/components/Seedling.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <div>
+        <h1 class="font-normal text-3xl leading-none">Рассада</h1>
+    </div>
+
+</template>
+
+<script>
+
+    export default {};
+
+</script>
