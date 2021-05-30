7d6193aa90e6213acedfa34118ba38dd4cc8a11f
agb2008 <64597413+agb2008@users.noreply.github.com>
2021-05-30 14:31:16 +0300

    Убраны папки ventor и node_modules
    Убраны ошибочно загруженные рабочие папки vendor и node_modules (из-за неправильной отработки файла
    .gitignore)

--- backend/vendor/infureal/artisan-gui/resources/views/partials/scripts.blade.php
+++
@@ -1,7 +0,0 @@
-@if(file_exists(public_path($styleFile = 'vendor/artisan-gui/gui.js')))
-    <script defer src="{{ asset($styleFile) }}"></script>
-@else
-    <script defer>
-        <?php include $guiRoot . '/stubs/js/gui.js'; ?>
-    </script>
-@endif
--- backend/vendor/infureal/artisan-gui/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Infureal\Http\Controllers\GuiController;
-
-\Route::get('/', [GuiController::class, 'index'])
-    ->name('gui.index');
-
-\Route::post('{command}', [GuiController::class, 'run'])
-    ->name('gui.run');
